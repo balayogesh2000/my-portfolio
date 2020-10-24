a70ce222793a0b97186a2278a382a12a8f67d166
balayogesh2000 <54364965+balayogesh2000@users.noreply.github.com>
2020-10-24 22:43:33 +0530

    Add files via upload

---
+++ precache-manifest.234401a243398d0a6d00d3c4f46f3240.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0c3d241992a0e3a88c9d808d9d080fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aef9cb9d98bbd639728a",
+    "url": "/static/css/main.6217d213.chunk.css"
+  },
+  {
+    "revision": "da851734b440092f80ca",
+    "url": "/static/js/2.1d90d80a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1d90d80a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aef9cb9d98bbd639728a",
+    "url": "/static/js/main.cdffa56a.chunk.js"
+  },
+  {
+    "revision": "24519fe60e427ac4b7ce",
+    "url": "/static/js/runtime-main.3c43cf27.js"
+  },
+  {
+    "revision": "f76169ba0e2b7b5fa40223f092b7859a",
+    "url": "/static/media/annotation.f76169ba.svg"
+  },
+  {
+    "revision": "e6aabea1bab56914a053c6d2a811b418",
+    "url": "/static/media/coder.e6aabea1.png"
+  },
+  {
+    "revision": "60fc139e9365e4618ed25d0e4fc7e16e",
+    "url": "/static/media/ecomm.60fc139e.JPG"
+  },
+  {
+    "revision": "159ae1c5050fd05506c546d33f093846",
+    "url": "/static/media/feeling_proud.159ae1c5.svg"
+  },
+  {
+    "revision": "8b7dd04770590e7560fa827d97b042ee",
+    "url": "/static/media/maze.8b7dd047.JPG"
+  },
+  {
+    "revision": "c6f811a8c6a11a2975acbf53ac1566c6",
+    "url": "/static/media/movieFight.c6f811a8.JPG"
+  },
+  {
+    "revision": "e2a914671640c420a53efc6c41a5b288",
+    "url": "/static/media/showcase.e2a91467.jpg"
+  },
+  {
+    "revision": "76c0035132e08185b483a4938f802cca",
+    "url": "/static/media/stepping_up.76c00351.svg"
+  }
+]);
